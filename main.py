67bba3efdf1355057493768d7fcc9cac2135e567
weekbin <18717744742@163.com>
2019-03-30 14:52:43 +0800

    add  console in the main function; rewrite the README.md ; change at 2019年3月30日14:52:41

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 import time
 
 import schedule
@@ -15,42 +16,68 @@ class Access(object):
     """
     main function
     """
-    def __init__(self, url_list, driver):
+    def __init__(self, url_list, driver, sleeping, rating):
         """
-        
+        Passing the main parameters
         """
         self.url_list = url_list
         self.driver = driver
+        self.sleeping = sleeping
+        self.rating = rating
+
+    def random_rate(self):
+        """
+        A number between 0 and 1 indicates probability
+        eg: 0.8 means 80% posibility
+        """
+        r = random.random()
+        if r <= self.rating:
+            return True
+        else:
+            return False
     
     def access(self):
         """
-        
+        Loop through the sites in the url access list
         """
-        j = 1
-        k = 1
-        sleeping = 10
+        sleeping = self.sleeping
         driver = self.driver
-        while True:
-            try:
-                for i in self.url_list:
+        try:
+            for i in self.url_list:
+                if self.random_rate():
                     try:
                         driver.get(i)
                         print(i + " is connected successfully")
                         time.sleep(2)
                     except:
-                        pass
-                print("成功访问"+"(" + str(j) + ")次")
-                j += 1
-                driver.quit()
-                print("Next access will be " + str(sleeping) + " later")
-                time.sleep(sleeping)
-            except:
-                print("访问出错"+"(" + str(k) + ")次")
-                k += 1
-                driver.quit()
-                time.sleep(5)
+                        print(i + " is connected failed")
+                else:
+                    print(i + " is skipped")
+            driver.quit()
+            return True
+        except:
+            driver.quit()
+            time.sleep(2)
+            return False
 
 if __name__ == "__main__":
-    driver = set_driver()
-    Connection = Access(url_list, driver)
-    Connection.access()
+    j = 0
+    k = 0
+    while True:
+        """
+        Declaration the main parameters
+        """
+        driver = set_driver()
+        sleeping = 10
+        rating = 0.5
+        Connection = Access(url_list, driver, sleeping, rating)
+        result = Connection.access()
+        print(result)
+        if result == True:
+            j += 1
+            print("成功访问"+"(" + str(j) + ")次")
+            print("Next access will be " + str(sleeping) + " seconds later")
+            time.sleep(sleeping)
+        else :
+            k += 1
+            print("访问出错"+"(" + str(k) + ")次")
--- url.py
+++ url.py
@@ -5,5 +5,16 @@ return: url list
 url_list = [
     "http://www.weekbin.xyz",
     "http://www.weekbin.xyz/register",
-    "http://www.weekbin.xyz/login"
+    "http://www.weekbin.xyz/login",
+    'http://www.tamakooo.com/', 
+    'http://www.tamakooo.com/books1', 
+    'http://www.tamakooo.com/books2', 
+    'http://www.tamakooo.com/books3', 
+    'http://www.tamakooo.com/books4', 
+    'http://www.tamakooo.com/books5', 
+    'http://www.tamakooo.com/books6', 
+    'http://www.tamakooo.com/look_book1', 
+    'http://www.tamakooo.com/look_book2', 
+    'http://www.tamakooo.com/look_book3', 
+    'http://www.tamakooo.com/look_book4'
     ]
